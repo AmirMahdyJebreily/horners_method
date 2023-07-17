fab0dbd25d13a3516d22f1f2af9a6b2348e9f162
Amir JCS <amirmahdyjdx@gmail.com>
2023-07-18 00:33:48 +0330

    feat: complete horners method base algorythm

---
+++ HornersMethod/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Enter Coefficients Here : ");
+float[] conffs = Console.ReadLine().Split(", ").Select(a => float.Parse(a)).ToArray();
+int grd = conffs.Length - 1;
+Console.Write("Enter origin of expertion : ");
+float org = float.Parse(Console.ReadLine());
+
+float[] xs = new float[conffs.Length];
+float[] res = new float[conffs.Length];
+
+for (int i = 0; i < conffs.Length; i++)
+{
+    res[i] = (conffs[i] + xs[i]);
+    if (conffs.Length - i != 1)
+    {
+        xs[i + 1] = res[i] * org;
+    }
+}
+
+int lgrd = grd - 1;
+foreach (float a in res[0..(res.Length - 1)])
+{
+    Console.Write($"{((a > 0)? "+" : "-")} {a}{((lgrd > 0)? ((lgrd > 1)? "x^" + lgrd : "x") : "")} ");
+    lgrd--;
+}
